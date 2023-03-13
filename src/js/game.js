9d2503c5b53f7469669f95165e763c18451a5bcf
Romain Aroldi <romain.aroldi57@gmail.com>
2023-03-13 14:56:32 +0100

    Add computer choice in the interface

--- src/js/game.js
+++ src/js/game.js
@@ -1,35 +1,84 @@
 window.addEventListener("load", () => {
 
-    const possibleChoices = ["Rock", "Paper", "Scissors"];
-    const resultText = document.querySelector("div#result h3");
-    const choiceContainer = document.querySelector("div#choice");
-    const choiceButtons = choiceContainer.querySelectorAll("button");
+    const possibleChoices = ["rock", "paper", "scissors"];
+    const resultText = document.querySelector("div#result h4");
+    const userChoiceContainer = document.querySelector("div#user-choice");
+    const computerChoiceContainer = document.querySelector("div#computer-choice");
+    const choiceButtons = userChoiceContainer.querySelectorAll("button");
+    const scoreText = document.querySelector("div#result span#score");
+    let scoreCount = 0;
 
     function getRandomChoice() {
         return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
     }
 
+    function clearSelectedChoices() {
+        choiceButtons.forEach((button) => {
+            button.classList.remove("selected-choice");
+        });
+        computerChoiceContainer.querySelectorAll("button").forEach((button) => {
+            button.classList.remove("selected-choice");
+        });
+    }
+
+    function clearAll() {
+
+        let message;
+
+        if (scoreCount > 2) {
+            message = "Vous avez gagné la partie !";
+        } else {
+            message = "Vous avez perdu la partie !";
+        }
+
+        scoreCount = 0;
+        resultText.textContent = "";
+        scoreText.textContent = "0";
+
+        clearSelectedChoices();
+
+        return message;
+    }
+
     choiceButtons.forEach((button) => {
+
         button.addEventListener("click", (e) => {
+
             e.preventDefault();
+
             const userChoice = button.dataset.choice;
             const computerChoice = getRandomChoice();
+
+            clearSelectedChoices();
+            button.classList.add("selected-choice");
+            computerChoiceContainer.querySelector("button." + computerChoice).classList.add("selected-choice");
+
             if (userChoice === computerChoice) {
                 resultText.classList.remove("text-success");
                 resultText.classList.remove("text-danger");
                 resultText.textContent = "Egalité !";
             } else if (
-                (userChoice === "Rock" && computerChoice === "Scissors") ||
-                (userChoice === "Paper" && computerChoice === "Rock") ||
-                (userChoice === "Scissors" && computerChoice === "Paper")
+                (userChoice === "rock" && computerChoice === "scissors") ||
+                (userChoice === "paper" && computerChoice === "rock") ||
+                (userChoice === "scissors" && computerChoice === "paper")
             ) {
                 resultText.classList.remove("text-danger");
                 resultText.classList.add("text-success");
-                resultText.textContent = "Vous avez gagné !";
+                resultText.textContent = "Gagné !";
+                scoreCount++;
             } else {
                 resultText.classList.remove("text-success");
                 resultText.classList.add("text-danger");
-                resultText.textContent = "Vous avez perdu !";
+                resultText.textContent = "Perdu :(";
+                scoreCount--;
+            }
+
+            scoreText.textContent = scoreCount.toString();
+
+            if (scoreCount > 2 || scoreCount < -2) {
+                setTimeout(() => {
+                    alert(clearAll());
+                }, 50);
             }
         });
     });
