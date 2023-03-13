f59b26658f36a0e548b2957acf618ea037eb62e6
Romain Aroldi <romain.aroldi57@gmail.com>
2023-03-13 15:23:32 +0100

    Add history feature

--- src/js/game.js
+++ src/js/game.js
@@ -6,6 +6,7 @@ window.addEventListener("load", () => {
     const computerChoiceContainer = document.querySelector("div#computer-choice");
     const choiceButtons = userChoiceContainer.querySelectorAll("button");
     const scoreText = document.querySelector("div#result span#score");
+    const historyContainer = document.querySelector("div#history");
     let scoreCount = 0;
 
     function getRandomChoice() {
@@ -34,6 +35,9 @@ window.addEventListener("load", () => {
         scoreCount = 0;
         resultText.textContent = "";
         scoreText.textContent = "0";
+        while (historyContainer.firstChild) {
+            historyContainer.removeChild(historyContainer.lastChild);
+        }
 
         clearSelectedChoices();
 
@@ -50,13 +54,19 @@ window.addEventListener("load", () => {
             const computerChoice = getRandomChoice();
 
             clearSelectedChoices();
+
             button.classList.add("selected-choice");
             computerChoiceContainer.querySelector("button." + computerChoice).classList.add("selected-choice");
+
+            const historyBlock = document.createElement("div");
+            historyBlock.classList.add("history-block");
 
             if (userChoice === computerChoice) {
                 resultText.classList.remove("text-success");
                 resultText.classList.remove("text-danger");
                 resultText.textContent = "Egalité !";
+                historyBlock.style.backgroundColor = "#9e9e9e";
+                historyBlock.textContent = "N";
             } else if (
                 (userChoice === "rock" && computerChoice === "scissors") ||
                 (userChoice === "paper" && computerChoice === "rock") ||
@@ -66,14 +76,20 @@ window.addEventListener("load", () => {
                 resultText.classList.add("text-success");
                 resultText.textContent = "Gagné !";
                 scoreCount++;
+                historyBlock.style.backgroundColor = "#2be061";
+                historyBlock.textContent = "G";
             } else {
                 resultText.classList.remove("text-success");
                 resultText.classList.add("text-danger");
                 resultText.textContent = "Perdu :(";
                 scoreCount--;
+                historyBlock.style.backgroundColor = "#e0372b";
+                historyBlock.textContent = "P";
             }
 
             scoreText.textContent = scoreCount.toString();
+
+            historyContainer.append(historyBlock);
 
             if (scoreCount > 2 || scoreCount < -2) {
                 setTimeout(() => {
